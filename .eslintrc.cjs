35b8a7f811bfd54a497466df1e1ad9dc6e194591
champi-dev <danielsarcor@gmail.com>
2024-06-18 13:23:21 -0500

    0: Husky, standard config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,16 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
     'standard-with-typescript',
     'plugin:react/recommended',
-    'prettier'
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
@@ -16,11 +19,26 @@ module.exports = {
   },
   plugins: ['react-refresh', 'prettier'],
   rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: false,
+      },
+    ],
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
     'react/prop-types': 'off',
-    'prettier/prettier': 'error'
+    'react/react-in-jsx-scope': 'off', // Disable React in scope for new JSX transform
+    '@typescript-eslint/no-non-null-assertion': 'off', // Allow non-null assertion
+    '@typescript-eslint/explicit-function-return-type': 'off', // Disable explicit return type rule
+    '@typescript-eslint/no-confusing-void-expression': 'off', // Allow void expressions in arrow functions
   },
-};
+  settings: {
+    react: {
+      version: 'detect', // Automatically detect the React version
+    },
+  },
+}
