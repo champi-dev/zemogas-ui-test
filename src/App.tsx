294a25cbdce5732d7a4b66745d481aab737e59d8
champi-dev <danielsarcor@gmail.com>
2024-06-18 19:56:01 -0500

    1: SpeakOut

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css'
-import { Nav, Header } from './components'
+import { Nav, Header, SpeakOut } from './components'
 
 function App() {
   return (
@@ -8,31 +8,7 @@ function App() {
       <Nav />
       <Header />
       <div className="max-centered">
-        <aside
-          className="banner banner-top"
-          role="doc-tip"
-          aria-label="Speak Out"
-        >
-          <div className="banner__left">
-            <span className="banner__hairline">Speak out. Be heard.</span>
-            <span className="banner__title">Be counted</span>
-          </div>
-          <div className="banner__right">
-            <p className="banner__text">
-              Rule of Thumb is a crowd sourced court of public opinion where
-              anyone and everyone can speak out and speak freely. It’s easy: You
-              share your opinion, we analyze and put the data in a public
-              report.
-            </p>
-          </div>
-          <button className="icon-button" aria-label="close">
-            <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg">
-              <g stroke="#000" strokeWidth="2" fill="none" fillRule="evenodd">
-                <path d="M1 19L19 1M1 1l18 18" />
-              </g>
-            </svg>
-          </button>
-        </aside>
+        <SpeakOut />
         <main role="main">
           {/* <!-- Start: Implementation -->
             👉 Your code goes here 👈
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,4 @@
 import Nav from './Nav'
 import Header from './Header'
-export { Nav, Header }
+import SpeakOut from './SpeakOut'
+export { Nav, Header, SpeakOut }
