9350b08294e94c032fd4669f82fb7ee1ddb73de4
champi-dev <danielsarcor@gmail.com>
2024-06-20 08:36:00 -0500

    10: Adding styles

--- src/components/PreviousRulings/components/SingleVotingCard/components/ListMode/index.tsx
+++ src/components/PreviousRulings/components/SingleVotingCard/components/ListMode/index.tsx
@@ -1,6 +1,7 @@
 import './styles.css'
 import { type SingleCelebrity } from '@/models'
 import { CardBottom } from '../index'
+import { timeAgo, truncateString } from '@/utils'
 
 interface ListModeProps {
   dataTestId: string
@@ -19,6 +20,36 @@ const ListMode = ({ dataTestId, celebrity }: ListModeProps) => {
 
       <div className="list-mode__winner-thumb">
         <img src="/assets/img/thumbs-down.svg" alt="thumbs up" />
+      </div>
+
+      <div className="list-mode__content">
+        <div className="content-left">
+          <span className="celebrity-name">{celebrity.name}</span>
+          <span className="celebrity-description">
+            {truncateString(celebrity.description)}
+          </span>
+        </div>
+
+        <div className="content-right">
+          <span className="time-ago">
+            {`${timeAgo(celebrity.createdAt)} in `}
+            <span className="capitalize">{celebrity.category}</span>
+          </span>
+
+          <div className="top-actions">
+            <div className="img-container" data-testid="thumbs-up">
+              <img src="/assets/img/thumbs-up.svg" alt="thumbs up" />
+            </div>
+
+            <div className="img-container right" data-testid="thumbs-down">
+              <img src="/assets/img/thumbs-down.svg" alt="thumbs down" />
+            </div>
+
+            <button className="vote-btn" data-testid="vote-now">
+              Vote Now
+            </button>
+          </div>
+        </div>
       </div>
 
       <CardBottom dataTestId="gauge-bar" votes={celebrity.votes} />
